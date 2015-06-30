0d48590fd50e374dc01e22d3a7f3a08449f60769
TJHL <Guest@WTS11.local>
2015-06-29 18:29:21 -0700

    joke button

---
+++ src/ChuckleClicker.java
@@ -0,0 +1,41 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+
+public class ChuckleClicker implements ActionListener {
+ 
+JButton button;	
+JButton buddon;	
+public static void main(String[] args){
+	ChuckleClicker joke = new ChuckleClicker();
+	joke.makeButtons();
+}
+void makeButtons(){
+	JFrame frame= new JFrame();
+	frame.setVisible(true);
+	JPanel panel =new JPanel();
+	 button = new JButton();
+	 buddon = new JButton();
+	button.setText("Joke");
+	buddon.setText("Punchline");
+	button.addActionListener(this);
+	buddon.addActionListener(this);
+	panel.add(button);
+	panel.add(buddon);
+	frame.add(panel);
+	frame.pack();
+}
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	if(arg0.getSource() == button){
+	JOptionPane.showMessageDialog(null, "Why did the chicken cross the road?");}
+	if(arg0.getSource() == buddon){
+	JOptionPane.showMessageDialog(null, "I don't know, he got turned into roadkill before he told me.");}
+	
+}
+}
