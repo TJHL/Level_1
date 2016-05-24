4d83417a22b78cf971d893b26ee590d095a0e917
TJHL <league@WTS5.attlocal.net>
2016-05-23 18:28:26 -0700

    Bianary

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.regex.Pattern;
 
 import javax.swing.JButton;
@@ -6,13 +8,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-/**
- * We’re going to make an application that converts binary codes (8-bits) into letters. You’ll need a JFrame and a
- * JPanel. On the panel, put two text fields and a button. A text field is made like this: JTextField answer = new
- * JTextField(20); It may look like this or you might have a better layout in mind.
- */
-
-public class BinaryConverter {
+public class BinaryConverter implements ActionListener {
 	public static void main(String[] args) {
 		BinaryConverter thing = new BinaryConverter();
 	}
@@ -21,34 +17,45 @@ public class BinaryConverter {
 	JPanel panel = new JPanel();
 	JTextField input = new JTextField(20);
 	JTextField output = new JTextField(20);
-	JButton button = new JButton();
+	JButton button = new JButton("Convert");
+
+	String A;
+	String B;
 
 	BinaryConverter() {
 		frame.setVisible(true);
 		frame.add(panel);
 		panel.add(input);
 		panel.add(button);
-		// button.panel.add(output);
+		panel.add(output);
+		button.addActionListener(this);
 		frame.pack();
 	}
 
 	int convert(String binary) {
 		if (binary.length() != 8) {
-			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+			JOptionPane.showMessageDialog(null, "Enter 8 numbers");
 			return 0;
 		}
-		if (!Pattern.matches("\2", binary)) {
-			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+		if (!Pattern.matches("[01]{8}", binary)) {
+			JOptionPane.showMessageDialog(null, "Enter 1's and Zeros.");
 			return 0;
 		}
 		try {
 			int asciiValue = Integer.parseInt(binary, 2);
 			return asciiValue;
 		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
-			// question.setText("");
+			JOptionPane.showMessageDialog(null, "Enter Binary");
+			// question.setText("Hi");
 			return 0;
 		}
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		A = input.getText();
+		convert(A);
+		output.setText(A);
+
 	}
 
 }
